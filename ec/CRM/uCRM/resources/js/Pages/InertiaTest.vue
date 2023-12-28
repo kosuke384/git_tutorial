b3a17babca47e78a5fca40f78a0834e5dbbfc984
kosuke384 <hayakawakousuke252@gmail.com>
2023-12-28 18:24:41 +0900

    inertia

---
+++ ec/CRM/uCRM/resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+</script>
+
+<template>
+Inertiaテスト
+<a href="/">aタグ経由</a>
+<Link href="/">リンク経由</Link>
+<Link :href="route('inertia.index')">名前付きルート</Link>
+<Link :href="route('inertia.show',{id:1})">ルートパラメーター</Link>
+</template>
