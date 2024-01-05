1900a5d3881e9aad9352951e51b86857c325bcb8
kosuke384 <hayakawakousuke252@gmail.com>
2024-01-05 13:16:24 +0900

    javascript練習

---
+++ ec/javascriot/main.js
@@ -0,0 +1,35 @@
+/*
+const postalCode='123-4567';
+
+const postal={
+    postalCode:'123-4567',
+    checkPostalCode(string){
+        const replaced=this.postalCode.replace('-','').length;
+        //const length=replaced.length;
+
+        if(replaced===7){
+            return true;
+        }
+        return false;
+    }
+}
+
+console.log(postal.checkPostalCode(postalCode));
+*/
+/*
+const person={
+    name:'名前',
+    age:20,
+    getName(){
+        console.log(this.name);
+        return this;
+    },
+    getAge(){
+        console.log(this.age);
+        return this;
+    }
+};
+
+person.getName().getAge();
+*/
+
