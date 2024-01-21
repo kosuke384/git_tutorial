e709e2147ce57f8c22f7eb91de5c36010da6a8ca
kosuke384 <hayakawakousuke252@gmail.com>
2024-01-21 23:57:42 +0900

    顧客登録　郵便番号検索機能追加

--- ec/CRM/uCRM/app/Http/Controllers/CustomerController.php
+++ ec/CRM/uCRM/app/Http/Controllers/CustomerController.php
@@ -29,7 +29,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Customers/create');
     }
 
     /**
@@ -37,7 +37,35 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        $request->validate([
+            'name'=>['required','max:20'],
+            'kana'=>['required','regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u','max:20'],
+            'tel'=>['required','max:20','unique:customers,tel'],
+            'email'=>['required','email','max:255','unique:customers,email'],
+            'postcode'=>['required','max:7'],
+            'address'=>['required','max:100'],
+            'birthday'=>['date'],
+            'gender'=>['required'],
+            'memo'=>['max:1000'],
+        ]);
+
+        Customer::create([
+            'name'=>$request->name,
+            'kana'=>$request->kana,
+            'tel'=>$request->tel,
+            'email'=>$request->email,
+            'postcode'=>$request->postcode,
+            'address'=>$request->address,
+            'birthday'=>$request->birthday,
+            'gender'=>$request->gender,
+            'memo'=>$request->memo
+        ]);
+
+        return to_route('customers.index')
+        ->with([
+            'message'=>'登録しました',
+            'status'=>'success'
+        ]);
     }
 
     /**
